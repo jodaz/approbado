0bd670154b7285dee10e90975f7822e2357489ee
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-10-18 22:51:31 -0400

    Update user password

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { APP_PORT, cors, helmet, APP_ENV } from './config'
-import { apiRoutes, webRoutes } from './routes'
+import routes from './routes'
 import path from 'path'
 
 // Set up server
@@ -12,16 +12,8 @@ app.use(express.json())
 // Static routes
 app.use('/static', express.static(path.resolve(__dirname, '../public')));
 
-// Auth iframe
-if (APP_ENV === 'testing') {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public', 'index.html'))
-    })
-}
-
 // Routes
-app.use(apiRoutes);
-app.use(webRoutes);
+app.use(routes);
 
 app.listen(APP_PORT, () => {
     console.log(`Application started on port ${APP_PORT}!`);
