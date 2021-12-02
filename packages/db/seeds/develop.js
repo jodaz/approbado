c2b15df054045614674e5f4ff9b0c703f3cf5bd0
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-12-02 08:12:26 -0400

    Fix dev seeder

--- packages/db/seeds/develop.js
+++ packages/db/seeds/develop.js
@@ -1,40 +1,39 @@
 //@ts-nocheck
-import { USER } from '@approbado/server/dist/config'
+import { USER, APP_ENV  } from '@approbado/server/dist/config'
 import { User, Trivia, Category, Level } from '@approbado/server/dist/models'
 import bcrypt from 'bcrypt'
 
-const categories = ['Comercio', 'Ambiente', 'Tributo'];
-const levels = ['Principiante', 'Intermedio', 'Avanzado'];
-
 export async function seed(knex) {
-  const encryptedPassword = await bcrypt.hash(USER.password, 10);
+    if (APP_ENV === 'development') {
+        const encryptedPassword = await bcrypt.hash(USER.password, 10);
 
-    // Create user test
-    await User.query().insert({
-        names: 'Test User',
-        user_name: 'test',
-        password: encryptedPassword,
-        is_registered: true,
-        email: 'user@ejemplo.com'
-    })
+        // Create user test
+        await User.query().insert({
+            names: 'Test User',
+            user_name: 'test',
+            password: encryptedPassword,
+            is_registered: true,
+            email: 'user@ejemplo.com'
+        })
 
-    await Category.query().insert({
-        name: 'Comercio',
-    })
+        await Category.query().insert({
+            name: 'Comercio',
+        })
 
-    await Level.query().insert({
-        name: 'Ambiente',
-    })
+        await Level.query().insert({
+            name: 'Ambiente',
+        })
 
-    const trivia = await Trivia.query().insert({
-        name: 'Derecho comercial',
-        category_id: 1,
-        level_id: 1,
-        is_free: 1
-    });
+        const trivia = await Trivia.query().insert({
+            name: 'Derecho comercial',
+            category_id: 1,
+            level_id: 1,
+            is_free: 1
+        });
 
-    trivia.$relatedQuery('subthemes').insert({
-        'name': 'Subtema',
-        'duration': 30
-    })
+        trivia.$relatedQuery('subthemes').insert({
+            'name': 'Subtema',
+            'duration': 30
+        })
+    }
 };
