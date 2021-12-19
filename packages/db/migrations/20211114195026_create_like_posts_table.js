cacf7660cb106234971eead5c4eff5d6bf51822b
Arcadio Arevalo <arevaloarcadio.96@gmail.com>
2021-12-19 18:41:37 -0400

    funciones de trivias (pendiente)

--- packages/db/migrations/20211114195026_create_like_posts_table.js
+++ packages/db/migrations/20211114195026_create_like_posts_table.js
@@ -1,11 +1,10 @@
 
-
 export async function up(knex) {
-    return knex.schema.createTable('participants', table => {
+    return knex.schema.createTable('like_posts', table => {
         table.increments('id').primary();
+        table.integer('post_id').unsigned();
         table.integer('user_id').unsigned();
-        table.integer('schedule_id').unsigned();
-        table.foreign('schedule_id').references('schedules.id').onDelete('cascade');
+        table.foreign('post_id').references('posts.id').onDelete('cascade');
         table.foreign('user_id').references('users.id').onDelete('cascade');
         table.timestamps();
     });
@@ -13,5 +12,5 @@ export async function up(knex) {
 
 
 export async function down(knex) {
-    return knex.schema.dropTable('participants')
+    return knex.schema.dropTable('like_posts')
 }
