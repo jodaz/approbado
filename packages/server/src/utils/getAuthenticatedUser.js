3551780ba372f5017bda019b57581329ff821c31
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-02-22 23:44:10 -0400

    Create get plan hook

--- packages/server/src/utils/getAuthenticatedUser.js
+++ packages/server/src/utils/getAuthenticatedUser.js
@@ -5,6 +5,9 @@ export const getAuthenticatedUser = async (user) => {
         profile.discussion = await user.$relatedQuery('posts').whereRaw('parent_id is null');
         profile.comments = await user.$relatedQuery('posts').whereRaw('parent_id is not null');
         profile.awards = await user.$relatedQuery('awards').withGraphFetched('trivia');
+        profile.memberships = await user.$relatedQuery('memberships')
+            .withGraphFetched('plans')
+            .where('active', '=', true);
 
         return profile;
     }
