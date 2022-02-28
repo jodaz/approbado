d43ad03fccb3137e16db400343f82825fce0f81f
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-02-28 02:57:04 -0400

    Layout trivias

--- packages/server/src/routes/files.routes.js
+++ packages/server/src/routes/files.routes.js
@@ -1,13 +1,19 @@
 import { Router } from "express"
 import { upload } from '../config'
-import { destroy, index, show, store, update } from '../controllers/FileController'
-import { createFileSchema } from '../validations'
-import { checkSchema } from 'express-validator';
+import {
+    destroy,
+    index,
+    show,
+    store,
+    download,
+    update
+} from '../controllers/FileController'
 
 const filesRouter = Router()
 
 filesRouter.get('/', index)
 filesRouter.get('/:id', show)
+filesRouter.get('/download/:id', download)
 filesRouter.post('/', upload.single('file'), store)
 filesRouter.put('/:id', upload.single('file'), update)
 filesRouter.delete('/:id', destroy)
