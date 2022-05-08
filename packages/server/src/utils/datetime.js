4e1dbfeef37562acc1036bd72df16632f2e02437
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-05-08 14:38:09 -0400

    Fix bugs

--- packages/server/src/utils/datetime.js
+++ packages/server/src/utils/datetime.js
@@ -11,22 +11,22 @@ export const  getDateString =  (value) => {
     }
     if (date.substr(0,3) == 'Mon') {
       day_week = 'LUN'
-    } 
+    }
     if (date.substr(0,3) == 'Tue') {
       day_week = 'MAR'
     }
     if (date.substr(0,3) == 'Wed') {
       day_week = 'MIE'
-    }   
+    }
     if (date.substr(0,3) == 'Thu') {
       day_week = 'JUE'
-    } 
+    }
     if (date.substr(0,3) == 'Fri') {
       day_week = 'VIE'
-    } 
+    }
     if (date.substr(0,3) == 'Sat') {
       day_week = 'SAB'
-    } 
+    }
 
     if (new Date(value).getMonth()+1 == 1) {
       month = 'ENERO'
@@ -69,7 +69,7 @@ export const  getDateString =  (value) => {
 }
 
 export const getDayWeekString =  (value,day)  =>{
-  
+
   let date = new Date(value).toDateString()
   let day_week = ''
 
@@ -79,52 +79,51 @@ export const getDayWeekString =  (value,day)  =>{
   }
   if (date.substr(0,3) == 'Mon') {
     day_week = 'Lunes'
-  } 
+  }
   if (date.substr(0,3) == 'Tue') {
     day_week = 'Martes'
   }
   if (date.substr(0,3) == 'Wed') {
     day_week = 'Miércoles'
-  }   
+  }
   if (date.substr(0,3) == 'Thu') {
     day_week = 'Jueves'
-  } 
+  }
   if (date.substr(0,3) == 'Fri') {
     day_week = 'Viernes'
-  } 
+  }
   if (date.substr(0,3) == 'Sat') {
     day_week = 'Sábado'
-  } 
-  
+  }
+
   return day_week+' '+day;
 }
 
 
 export const getTimeString =  (value)  =>{
+    let date = new Date(value);
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    let dt = date.getDate();
 
-  let date = new Date(value);
-  let year = date.getFullYear();
-  let month = date.getMonth()+1;
-  let dt = date.getDate();
+    if (dt < 10) {
+        dt = '0' + dt;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
 
-  if (dt < 10) {
-    dt = '0' + dt;
-  }
-  if (month < 10) {
-    month = '0' + month;
-  }
-  
-  let TimeString = new Date(value).toLocaleTimeString()
-  let day_week = ''
-  let time = TimeString.split(':') 
-  let response;
-  
-  if (parseInt(time[0]) > 12) {
-    response = parseInt(time[0]) - 12+' PM'
-  }else{
-    response = time[0] +' AM'
-  }
-  
-  return response;
+    let TimeString = new Date(value).toLocaleTimeString()
+    let day_week = ''
+    let time = TimeString.split(':')
+    let response;
+
+    if (parseInt(time[0]) > 12) {
+        response = parseInt(time[0]) - 12+' PM'
+    }else{
+        response = time[0] +' AM'
+    }
+
+    return response;
 }
 
